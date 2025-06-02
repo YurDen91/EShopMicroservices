e5ddc8489f015ba9f40a47ac0011c0731db90f7f
YurDen91 <yurden20@gmail.com>
2025-06-02 10:15:45 +0300

    Implement DeleteBasket endpoint and command handling with validation

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,24 @@
 namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
-    
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required.");
+    }
+}
+
+public class DeleteBasketHandler(IDocumentSession session) : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+    {
+        //ToDo: delete the basket from the database and cache
+        //session.Delete<ShoppingCart>(request.UserName);
+        
+        return new DeleteBasketResult(true);
+    }
 }
