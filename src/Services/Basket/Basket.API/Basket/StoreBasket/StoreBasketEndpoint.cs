8f630397952ac6ace43e6c5d0777cbcb9bd8858c
YurDen91 <yurden20@gmail.com>
2025-06-02 10:06:24 +0300

    Implement StoreBasket endpoint and command handling with validation

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,6 +1,25 @@
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketEndpoint
+public record StoreBasketRequest(ShoppingCart Cart);
+
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = new StoreBasketCommand(request.Cart);
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<StoreBasketResponse>();
+
+            return Results.Created($"/basket{response.UserName}", response);
+        })
+        .WithName("StoreBasket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithTags("Basket");
+    }
 }
