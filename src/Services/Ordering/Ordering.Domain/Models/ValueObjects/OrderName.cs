53d7898d5578bd15ff7d8d0a7da28032a4de9403
YurDen91 <yurden20@gmail.com>
2025-08-11 09:32:01 +0300

    Add factory methods for Address, OrderId, OrderName, Payment, and ProductId records

--- src/Services/Ordering/Ordering.Domain/Models/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/Models/ValueObjects/OrderName.cs
@@ -2,5 +2,15 @@ namespace Ordering.Domain.Models.ValueObjects;
 
 public record OrderName
 {
+    private const int DefaultLength = 5;
     public string Value { get; set; }
+    private OrderName(string value) => Value = value;
+    
+    public static OrderName Of(string value)
+    {
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+
+        return new OrderName(value);
+    }
 }
--- src/Services/Ordering/Ordering.Domain/Models/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/Models/ValueObjects/Payment.cs
@@ -7,4 +7,33 @@ public record Payment
     public string Expiration { get; init; } = default!;
     public string CVV { get; init; } = default!;
     public int PaymentMethod { get; init; } = default!;
+    
+    protected Payment() { }
+    
+    private Payment(string cardNumber,
+        string cardName,
+        string expiration,
+        string cvv,
+        int paymentMethod)
+    {
+        CardNumber = cardNumber;
+        CardName = cardName;
+        Expiration = expiration;
+        CVV = cvv;
+        PaymentMethod = paymentMethod;
+    }
+    
+    public static Payment Of(string cardNumber,
+        string cardName,
+        string expiration,
+        string cvv,
+        int paymentMethod)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(cvv.Length, 3);
+        
+        return new Payment(cardNumber, cardName, expiration, cvv, paymentMethod);
+    }
 }
